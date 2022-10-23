6724121bec657dbe67b9003544302c5980ce26aa
Damir021 <k.damir021@gmail.com>
2022-10-24 04:56:00 +0600

    add page

---
+++ sendmail.php
@@ -0,0 +1,43 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	$mail->setFrom('ITS@site.ru', 'Сайт заглушка');
+
+	$mail->addAddress('k.damir021@gmail.com');
+
+	$mail->Subject = 'Форма обратной связи';
+
+	$body = '';
+
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['tel']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['tel'].'</p>';
+	}
+
+	$mail->Body = $body;
+
+	if(!$mail->send()){
+		$message = 'Ошибка';
+	}else{
+		$message = 'Данные отправленны!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response)
+?>
